49f17b29459d903dab7cc883e972b84ca5389929
Aramid Mauricio Monsalve Usuga <aramidps4@gmail.com>
2026-09-10 10:52:38 -0500

    master to main

--- SportsLeague/SportsLeague.DataAccess/obj/Debug/net10.0/SportsLeague.DataAccess.AssemblyInfo.cs
+++ SportsLeague/SportsLeague.DataAccess/obj/Debug/net10.0/SportsLeague.DataAccess.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SportsLeague.DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96244b778f871b52bbb46800a89ccca8293282e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("SportsLeague.DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SportsLeague.DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
